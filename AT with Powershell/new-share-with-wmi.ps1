9032c1df48684a4ebe6173952bf7d5ecb32ed144
Mykhayl Puzanov <barbos@inbox.ru>
2022-01-22 21:52:18 +0300

    Create files share with WMI/CIM

---
+++ AT with Powershell/new-share-with-wmi.ps1
@@ -0,0 +1,15 @@
+﻿# Only for "Run as administrator" mode
+([wmiclass]'Win32_Share').Create("C:\TestACL", "Test", 3)
+
+# Variant 2 : it should work, but does not
+#$ppx = ([wmiclass]'Win32_Share').GetMethodParameters("Create")
+#$ppx.Name = "Test"
+#$ppx.Path = "C:\TestACL"
+#$ppx.Type = 0
+#$res = Invoke-WmiMethod -Class Win32_Share -Name Create -ArgumentList $ppx, $null
+
+# Variant 3: The code below seems to work (only in 'RiA' mode)
+$res = Invoke-CimMethod -ClassName Win32_Share -MethodName Create -Arguments @{Name = "games"; Path = "C:\games"; Type = [uint32]0}
+
+
+
